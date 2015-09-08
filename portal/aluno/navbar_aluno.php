a6156d993902db9ab0a0e7c5d438339090002e41
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-09-08 00:48:54 -0300

    Tela Visão do aluno e alterações

--- portal/aluno/navbar_aluno.php
+++ portal/aluno/navbar_aluno.php
@@ -5,14 +5,15 @@
                             Página Inicial </a>
                         </li>      
                         <li class="">
-                        <div class="btn-group block">
-                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> 
-                            Cadastrar <span class="caret"></span>
+                      <div class="btn-group block">
+                            <a class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="icon-user"> </i> 
+                            Arquivo TGSI <span class="caret"></span>
                             </a>                    
                             <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">             
                                 <li class="">
-                                    <a href="cadastrar.php" target="">
-                                         Usuário
+                                    <a href="enviar-arquivo.php" target="">
+                                         Enviar Arquivo
                                     </a>
                                 </li>  
                             </ul>
@@ -22,129 +23,29 @@
                         <li class="">
                         <div class="btn-group block">
                             <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-edit"></i> 
-                            Gerenciar <span class="caret"></span>
+                            Resultado Avaliações <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">    
                                 <li class="">
                                     <a href="https://portal.ufsm.br/aluno/alteracao-curricular/solicitaTrancamentoTotal.html" target="">
-                                        Turmas
+                                        Proposta
                                     </a>
                                 </li>
             
                                 <li class="">
                                     <a href="https://portal.ufsm.br/aluno/requerimento/dispensa.html" target="">
-                                        Bancas
+                                        TGSI
                                     </a>
                                 </li>  
                             </ul>
                         </div>
                         </li>
-    
-
-    
-                        <li class="">
-                        <div class="btn-group block">
-                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-table"></i>  
-                            Pesquisa <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                               <li class="disabled">
-                                    <a>
-                                        Teste
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        </li>
-
-    
-                        <li class="">
-                        <div class="btn-group block">
-                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-print"></i>  
-                            Relatórios <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/atestadoTrancamento/index.html" target="">
-                                        Formulário para Definição do Tema e Professor Orientador 
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/provavelFormando/index.html" target="">
-                                        Ficha de Avaliação do TGSI
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/reconhecimentoCurso/index.html" target="">
-                                        Ficha de Avaliação da Proposta para TGSI
-                                    </a>
-                                </li>                            
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/certificadoBolsista/index.html" target="">
-                                        Formulário para Confirmação da Entrega dos Artigos para a Banca de Avaliação
-                                    </a>
-                                </li>                            
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/certificadoBolsista/index.html" target="">
-                                        Formulário para Divulgação das Bancas de TGSI
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/comprovanteMatricula/index.html" target="">
-                                        Formulário para Confirmação da Entrega da Versão Final do TGSI
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/fichaCadastralAluno/index.html" target="">
-                                        Parecer do Orientador para Entrega dos Artigos à Banca de Avaliação
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/historicoEscolarSimplificado/index.html" target="">
-                                        Ficha de Acompanhamento das Orientações do TGSI
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/aluno/relatorio/matrizCurricular/index.html" target="">
-                                        Modelo de Proposta de TGSI
-                                    </a>
-                                </li>
                             </ul>
                         </div>
                         </li>    
 
     
-                        <li class="">
-                        <div class="btn-group block">
-                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-sitemap"></i>  
-                            Outros <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/biblioteca/index.html" target="">
-                                         Biblioteca
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="http://site.ufsm.br/ufsm/calendario-academico" target="_blank">
-                                         Calendário letivo
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/ru/usuario" target="">
-                                         Restaurante Universitário
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="https://portal.ufsm.br/prae/index.html" target="">
-                                         Benefício Socioeconômico
-                                    </a>
-                                </li>
-                                <li class="disabled">
-                                    <a>
-                                         Teste de suficiência em língua estrangeira
-                                    </a>
-                                </li>
+                        
                             </ul>
                         </div>
                         </li> 
