545730f400439d21dc8c5eed5a747bfca3aeeb48
Talliny Dalla Nora <loja.anima.animus@gmail.com>
2015-09-08 12:01:00 -0300

    Atualização visão orientador

--- portal/orientador/index.php
+++ portal/orientador/index.php
@@ -22,9 +22,54 @@
         <!-- main --> 
         <div class="band"> 
             <div class="container"> 
-                <div class="box info bordered shadowed margin-v rounded ">
-                Bem vindo! Escolha a opção desejada acima.
-                </div> 
+                 <div class="row">
+                    <div class="span4">
+                        <div class="box info bordered tip shadowed rounded">
+                            <br>
+                            <h2 class="align-center">Aluno 1</h2>
+                            <br>
+                            <div class="row">
+                                <div class="span1">
+                                    <div class="box success bordered tip shadowed rounded">
+                                        <span class="text align-center">Proposta</span>
+                                        <span class="text align-center">25/08/2015</span>
+                                    </div>
+                                </div>
+                                <div class="span1">
+                                    <div class="box warning bordered tip shadowed rounded">
+                                        <span class="text align-center">TGSI 1</span>
+                                        <span class="text align-center">25/09/2015</span>
+                                    </div> 
+                                </div>
+                                <div class="span1">
+                                    <div class="box control bordered tip shadowed rounded">
+                                        <span class="text align-center">TGSI 2</span>
+                                        <span class="text align-center">25/09/2015</span>
+                                    </div>
+                                </div>
+                            </div>  
+                            <br>
+                        </div>
+                    </div>
+                    <div class="span4">
+                        <div class="box info bordered tip shadowed rounded">
+                            <p class="align-center"><br>
+                                <h2 class="align-center">Aluno 2</h2>
+                                Proposta 1<br>
+                                25/08/2015<br>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="span4">
+                        <div class="box info bordered tip shadowed rounded">
+                            <p><br>
+                                <h2 class="align-center">Aluno 3</h2>
+                                <span class="text align-center">TGSI 1</span>
+                                <span class="text align-center">25/08/2015</span>
+                            <br></p>
+                        </div>
+                    </div>
+                </div>                
             </div>
         </div> 
         <ul class="vakata-context"></ul>
