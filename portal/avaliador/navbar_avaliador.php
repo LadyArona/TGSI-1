6b045dcf2a715c6c4ed3a0eb7751c192180d9a68
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-09-08 22:00:11 -0300

    criação da tela avaliador
    Inicio da criação da tela avaliador

---
+++ portal/avaliador/navbar_avaliador.php
@@ -0,0 +1,44 @@
+<nav class="band navbar gradient control">
+                <div class="container mini-padding-v">
+                    <ul class="nav responsive">
+                        <li class=""><a href="index.php" target=""><i class="icon-home"></i>  
+                            Página Inicial </a>
+                        </li>  
+                        
+                        <!-- Cadastrar -->
+                        <li class="">
+                        <div class="btn-group block">
+                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> 
+                            Avaliar <span class="caret"></span>
+                            </a>                    
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">             
+                                <li class="">
+                                    <a href="avaliar.php" target="">
+                                         Aluno
+                                    </a>
+                                </li>
+                                
+                                </ul>
+                        </div>
+                        </li>
+                        
+                        <!-- Relatórios -->
+                        
+                        <!-- Outros -->    
+                        <li class="">
+                        <div class="btn-group block">
+                            <a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-sitemap"></i>  
+                            Outros <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+                               
+                                     
+                               
+                              
+                            </ul>
+                        </div>
+                        </li> 
+                    </ul>
+                </div>
+    </nav>
+            
