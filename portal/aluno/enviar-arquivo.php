12d76920692a3a4c590f00bd30c927a542066ea0
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-09-14 10:17:28 -0300

    ajustes no enviar-arquivo do aluno
    Alterado o botão de enviar arquivo de aluno

--- portal/aluno/enviar-arquivo.php
+++ portal/aluno/enviar-arquivo.php
@@ -12,7 +12,7 @@
            
           
             <form id="formOcorrencia" action="/aluno/alteracao-curricular/trancamentoTotal.html" method="post" enctype="multipart/form-data"><div class="row"> 
-                    <div class="span12"> <span class="label">Observação</span>
+                    <div class="span12"> <span class="label">Observações</span>
 <br>
  <div class=""><textarea id="justificativa" name="justificativa" class="textarea" rows="5"></textarea><ul class="list-h inner-separated pull-right"><li>Restam 1024 caracteres</li><li>Caracteres: 0</li><li>Palavras: 0</li></ul></div>
  <span id="contadorJustificativa"></span>  
@@ -23,23 +23,21 @@
                 <fieldset class="bordered rounded shadowed margin-bottom"> 
                     <div class="row"> 
                         <div> É possível anexar documentos e enviar para seu orientador. <br> <strong>Tamanho arquivos:</strong> 10 Mb por arquivo. <br> 
-                           
-                    <legend class="bold">Anexos</legend> 
-                    
+                                
                     <table id="tableAnexos" class="table bordered rounded shadowed striped stroked narrow">
-                        <thead class="header"> <tr> 
-                                <th style="width: 3em;">#</th> <th style="width: 7em;">
-                                    <button id="addBtn" type="button" class="btn small inverse">+</button>
-                                </th> <th>Descrição</th> </tr> </thead> <tbody>  
+                        <thead class="header"> <tr>                   
+                               
+                    </th> <th>  <br> <input type="file" name="parquivo" id="parquivo" /> <br></th> </tr> </thead> <tbody>  
                         </tbody> 
-                    </table> </fieldset>                                        
+                    </table> </fieldset>   
+                
                 
                 <div class="form-actions bottom">
                     <button id="cancel" type="button" class="btn left">
                         <i class="icon-ban-circle"></i> Cancelar</button>
      <button id="salvar" type="button" class="btn primary" data-hasqtip="2">
          <i class="icon-save"></i> Enviar</button> </div></form>
-            
+             
            
 
 <?php
