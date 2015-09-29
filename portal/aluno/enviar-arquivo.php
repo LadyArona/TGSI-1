a02a262085b8e2d680b7bfad0ae3bd01c8b97827
Talliny Dalla Nora <loja.anima.animus@gmail.com>
2015-09-29 02:51:01 -0300

    upload dos arquivos

--- portal/aluno/enviar-arquivo.php
+++ portal/aluno/enviar-arquivo.php
@@ -3,43 +3,70 @@
     include("cabecalho.php");
     include("../navbar.php");
     include("navbar_aluno.php");
+    
+    $tipo = $_POST['tipo'];
 ?>
 
-    <!-- main -->
-    <div class="band">
-        <div class="container">
-            <h2 class="primary stroked-bottom text-shadowed margin-bottom "> Envio de arquivo para orientador</h2>
-           
-          
-            <form id="formOcorrencia" action="/aluno/alteracao-curricular/trancamentoTotal.html" method="post" enctype="multipart/form-data"><div class="row"> 
-                    <div class="span12"> <span class="label">Observações</span>
-<br>
- <div class=""><textarea id="justificativa" name="justificativa" class="textarea" rows="5"></textarea><ul class="list-h inner-separated pull-right"><li>Restam 1024 caracteres</li><li>Caracteres: 0</li><li>Palavras: 0</li></ul></div>
- <span id="contadorJustificativa"></span>  
-                    </div>
-                </div>
-              
-                
-                <fieldset class="bordered rounded shadowed margin-bottom"> 
-                    <div class="row"> 
-                        <div> É possível anexar documentos e enviar para seu orientador. <br> <strong>Tamanho arquivos:</strong> 10 Mb por arquivo. <br> 
-                                
-                    <table id="tableAnexos" class="table bordered rounded shadowed striped stroked narrow">
-                        <thead class="header"> <tr>                   
-                               
-                    </th> <th>  <br> <input type="file" name="parquivo" id="parquivo" /> <br></th> </tr> </thead> <tbody>  
-                        </tbody> 
-                    </table> </fieldset>   
-                
-                
-                <div class="form-actions bottom">
-                    <button id="cancel" type="button" class="btn left">
-                        <i class="icon-ban-circle"></i> Cancelar</button>
-     <button id="salvar" type="button" class="btn primary" data-hasqtip="2">
-         <i class="icon-save"></i> Enviar</button> </div></form>
-             
-           
+<script type="text/javascript">
+function blocTexto(valor)
+{
+    quant = 1024;
+    total = valor.length;
+    if(total <= quant)
+    {
+        resto = quant - total;
+        document.getElementById('cont').innerHTML = resto;
+    }
+    else
+    {
+        document.getElementById('texto').value = valor.substr(0,quant);
+    }
+}
+</script>
 
-<?php
-	include("../rodape.php");
-?>  
+<!-- main -->
+<div class="band">
+    <div class="container">
+        <h2 class="primary stroked-bottom text-shadowed margin-bottom "> Envio de arquivo <?php echo $tipo; ?> para orientador</h2>
+        <form id="formOcorrencia" action="arquivo-upload.php" method="POST" enctype="multipart/form-data">
+            <div class="row"> 
+                <div class="span12"> 
+                    <span class="label">Observações</span><br>
+                    <div class="">
+                        <textarea onkeyup="blocTexto(this.value)" id="texto" name="texto" class="textarea" rows="5" maxlength="1024"></textarea>
+                        <ul class="list-h inner-separated pull-right">
+                            <li>Restam <span id="cont">1024</span> caracteres</li>
+                        </ul>
+                    </div>  
+                </div>
+            </div>
+            <fieldset class="bordered rounded shadowed margin-bottom"> 
+            <div class="row"> 
+                <div>
+                    É possível anexar documentos e enviar para seu orientador. <br> 
+                    <strong>Tamanho arquivos:</strong> 2 Mb por arquivo. <br> 
+
+                    <table id="tableAnexos" class="table bordered rounded shadowed striped stroked narrow">
+                        <thead class="header"> 
+                            <tr>                   
+                                <th>
+                                    <input type="file" name="arquivo" id="arquivo" class="padding-v"/>
+                                </th> 
+                            </tr> 
+                        </thead>
+                    </table>
+                </div>
+            </div>
+            </fieldset> 
+            <div class="form-actions bottom">
+                <button id="cancel" type="button" class="btn left" onclick="parent.location='index.php'">
+                    <i class="icon-ban-circle"></i> Cancelar
+                </button>
+                <button id="salvar" type="Submit" class="btn primary">
+                    <i class="icon-save"></i> Enviar
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include("../rodape.php"); ?>  
