2aee65b515c10f04dea74342fd56325079e6d184
Talliny Dalla Nora <loja.anima.animus@gmail.com>
2015-09-29 20:53:01 -0300

    Index do Aluno

--- portal/aluno/index.php
+++ portal/aluno/index.php
@@ -31,50 +31,45 @@
         <!-- main --> 
         <div class="band align-center-phone">
             <div class="container"> 
-                <div class="box success bordered tip shadowed rounded">
+                <div class="box warning bordered tip shadowed rounded">
                     <div class="row">
                         <div class="span10">
                             <h2 class="primary stroked-bottom text-shadowed margin-bottom ">Proposta</h2>
-                            <div class="row">
+                              <div class="row">
                                 <div class="span6 padding-v">                            
-                                    <p>
-                                        Sua proposta foi aceita!<br>
-                                        Agora você deve enviar o seu TGSI 1.
-                                    </p>
+                                    <p>Aguardando envio!</p>
+                                    <br>  
                                 </div>
-                                <div class="span6 align-right padding-v align-center-phone"> 
-                                    <button class="btn link" id="baixar" name="baixar"  type="button" onclick="">
-                                        <i class="icon-download-alt"></i> Baixar Arquivo
-                                    </button>   
+                                <div class="span6 padding-v align-right align-center-phone">
+                                    <form name="gerar" method="POST" action="enviar-arquivo.php">
+                                        <input type="hidden" name="tipo" value="Proposta">
+                                        <button class="btn primary gerarBtn" id="gerar" name="gerar"  type="Submit">
+                                            <i class="icon-upload-alt"></i> Enviar Arquivo
+                                        </button>                                         
+                                    </form>
                                 </div>
                             </div>
                         </div>
                         <div class="span2 align-center padding-top">
-                            <h1 id='iconBig' class="success"><i class="icon-ok-circle"></i></h1>
+                            <h1 id='iconBig' class="warning"><i class="icon-ok-circle"></i></h1>
                         </div>
                     </div>
                 </div>
                 <p>
                 <h1 id='iconMed' class="align-center"><i class="icon-arrow-down"></i></h1>
                 </p>
-                <div class="box warning bordered tip shadowed rounded">                    
+                <div class="box info bordered tip shadowed rounded">                    
                     <div class="row">
                         <div class="span10">
                             <h2 class="primary stroked-bottom text-shadowed margin-bottom ">TGSI 1</h2>
                             <div class="row">
-                                <div class="span6 padding-v">                            
-                                    <p>Aguardando envio!</p>
-                                    <br>  
-                                </div>
-                                <div class="span6 padding-v align-right align-center-phone">
-                                    <button class="btn primary gerarBtn" id="gerar" name="gerar"  type="button" onclick="window.open('enviar-arquivo.php','_parent')">
-                                        <i class="icon-upload-alt"></i> Enviar Arquivo
-                                    </button> 
+                                <div class="span6 padding-v"> 
+                                   <p>Aguardando liberação.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="span2 align-center padding-top">
-                            <h1 id='iconBig' class="warning"><i class="icon-upload"></i></h1>
+                            <h1 id='iconBig' class="primary"><i class="icon-remove-circle"></i></h1>
                         </div>
                     </div>
                 </div> 
