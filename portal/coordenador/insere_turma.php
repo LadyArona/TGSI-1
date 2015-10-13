bda294747dace9a9bb21882093506ed8f849a24d
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-10-13 00:11:18 -0300

    Melhorias no cadastro da turma

--- portal/coordenador/insere_turma.php
+++ portal/coordenador/insere_turma.php
@@ -1,33 +1,37 @@
    <?php
-    include("../include/conexao.php");    
+    include("../include/conexao.php"); 
+//    include("../include/funcoes.php");
     
+     if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+     
+   //pegando os dados do formulário e inserindo na variável do php -- Trim é para tirar os espaços em branco no inicio e fim de frase
+   $ano = trim($_POST['ano']); 
+   $semestre= trim($_POST['semestre']);
+   $descricao = trim($_POST['descricao']);
+   $data_proposta  =  trim ($_POST['data_proposta']);
    
-//recebendo os dados do formulário
-    $ano = trim($_POST['ano']);
-    $semestre= trim($_POST['semestre']);
-    $descricao = trim($_POST['descricao']);
-    $data_proposta  =  trim ($_POST['data_proposta']);
-    
-        
-    $sql = "INSERT INTO turma ('tur_codigo','tur_ano',tur_semestre,'tur_descricao', 'tur_data_proposta')
-    VALUES ('".null."',".$ano."','".$semestre."', '$descricao', '$data_proposta');";
-//    $query = mysqli_query($sql) or die(mysqli_error());
-  $query = $mysqli->query($sql);   
-    echo "<script>alert('Turma cadastrada.');</script>";
-   
-//    $query = mysqli_query($sql);
-//
-//     if ($query == true) {
-//
-//            echo "Cadastro efetuado com sucesso!";
-//
-//        } else {
-//            // Não foi possível fazer o cadastro
-//            echo "Não foi possível cadastrar, tente novamente.";
-//        }
+    //colocando a consulta dentro da variável sql
+    $sql = "INSERT INTO `turma` (`tur_ano`, `tur_semestre`, `tur_descricao`, `tur_data_proposta`)
+     VALUES ($ano','$semestre','$descricao', '$data_proposta');";
 
-    ?>
-    
-    
-
-    
+    if (!$mysqli->query("SET a=1")) {
+        printf("Errormessage: %s\n", $mysqli->error);
+    }
+     
+    if (!mysqli_query($mysqli, "SET a=1")) {
+    printf("Errormessage: %s\n", mysqli_error($mysqli));
+}
+else{
+   $mysqli->query($sql);
+   echo "Seu cadastro foi realizado com sucesso!<br>";
+}
+     echo $ano,$semestre,$descricao,$data_proposta;
+      die();
+      
+/* fecha conexão */
+      $mysqli->close();
+?>
+     
