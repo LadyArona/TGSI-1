7de9708cc92b090b7ce04deb19b04580d7d15e19
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-10-13 00:12:20 -0300

    Melhorias turma

--- portal/coordenador/turma_seg.php
+++ portal/coordenador/turma_seg.php
@@ -20,8 +20,8 @@
                         <div class="span4">
                             <span class="label">Período<span class="required"></span></span><br>
                             <select class="selectfield" id="semestre" name="semestre" required>
-                                <option value="1">1. Semestre</option>
-                                <option value="2">2. Semestre</option>
+                                <option value="1">1. semestre</option>
+                                <option value="2">2. semestre</option>
                               </select>
                         </div>
                        
@@ -41,69 +41,6 @@
                 </div>
                 <br>
                 
-                <!-- tabela de alunos -->
-<!--                <div class="bordered rounded diced striped hovered shadowed narrow table">
-                    <table class="bordered rounded diced striped hovered shadowed narrow table">
-                        <caption>Alunos da turma</caption>
-                        <thead class="header"> <tr> <th WIDTH="80"></th> <th>Aluno</th> <th class="hidden-tablet">Título do Trabalho</th> <th class="hidden-tablet">Orientador</th> <th class="hidden-tablet">Coorientador</th> </tr> </thead>
-                        <tbody>
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td>
-                                <td>Aluno 1-1</td> 
-                                <td class="hidden-tablet">Título do trabalho do aluno 1</td> 
-                                <td class="hidden-tablet">Orientador 1-2</td> 
-                                <td class="hidden-tablet">Coorientador 1-3</td> 
-                            </tr>
-                            
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td>
-                                
-                                <td>Aluno 2-1</td> 
-                                <td class="hidden-tablet">Título do trabalho do aluno 2</td> 
-                                <td class="hidden-tablet">Orientador 2-2</td> 
-                                <td class="hidden-tablet"> </td> 
-                            </tr>
-                            
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td> 
-                                <td>Aluno 3-1</td>
-                                <td class="hidden-tablet">Título do trabalho do aluno 3</td> 
-                                <td class="hidden-tablet">Orientador 3-2</td> 
-                                <td class="hidden-tablet">Coorientador 3-3</td> 
-                            </tr>
-                        </tbody>
-                    </table>
-
-                </div>
-                -->
-
-                <div class="form-actions">
-                    <button class="btn left cancelBtn" id="cancelar" name="cancel" type="button" >
-                        <i class="icon-ban-circle"></i> Cancelar</button>
-                    <button class="btn left Reset" id="limpar" name="limpar" type="reset">
-                        <i class="icon-eraser"></i> Limpar</button>
-                    <button class="btn primary saveBtn" id="salvar" name="submit" type="submit">
-                        <i class="icon-save"></i> Salvar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
 <?php
 	include("../rodape.php");
 ?>
