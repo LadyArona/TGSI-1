86b9f674f604d3cff6ffed8fcc0d8518f842ba42
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-10-19 11:40:46 -0200

    Melhorias na turma

--- portal/coordenador/insere-turma.php
+++ portal/coordenador/insere-turma.php
@@ -6,31 +6,30 @@
        $descricao      = $mysqli->real_escape_string($_POST['descricao']);
        $data_proposta  = $mysqli->real_escape_string ($_POST['data_proposta']);
     
-       
-            $search = mysql_query("SELECT * FROM tabela3 WHERE ano = '$ano' AND semestre = '$semestre'");
-            if(@mysqli_num_rows($search) > 0){
+
+       $search = mysql_query("SELECT * FROM turma WHERE ano = '$ano' AND semestre = '$semestre'");
+            if(mysqli_num_rows($search) > 0){
             echo 'Essa turma já existe';
-            }else{   
-  
-            //colocando a consulta dentro da variável sql
-            $sql = "INSERT INTO `turma` (`tur_ano`, `tur_semestre`, `tur_descricao`, `tur_data_proposta`)
-             VALUES ('$ano','$semestre','$descricao', '$data_proposta');";
-
-            $res = $mysqli->query($sql);
+            }else{  
+                
             }
+//        
+//        //executa 
+        $sql = "INSERT INTO `turma` (`tur_ano`, `tur_semestre`, `tur_descricao`, `tur_data_proposta`)
+         VALUES ('$ano','$semestre','$descricao', '$data_proposta');";
 
-                if ($mysqli->connect_error) {
-                    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-                }    
+        $res = $mysqli->query($sql);
+        
 
-                if (mysqli_connect_errno()) {
-                printf("Connect failed: %s\n", mysqli_connect_error());
-                exit();
-                }
-
-            else{
-                header("Location: turma.php?mensagem=A turma ano $ano e $semestre semestre foi inserida com sucesso! "); 
-            }
-                die();    
-
-          ?>     
+        if ($mysqli->connect_error) {
+        die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+        }  
+        
+        if (mysqli_connect_errno()) {
+        printf("Falha ao se conectar: %s\n", mysqli_connect_error());
+        exit();
+}
+        header("Location: turma.php?mensagem=A turma ano $ano do $semestre º semestre foi inserida com sucesso! ");     
+        die();    
+//}
+      ?>     
