160f3b889d70486437b79ca260b426eddd728d29
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-10-19 14:16:15 -0200

    Criação do busca Turma

---
+++ portal/coordenador/busca-turma.php
@@ -0,0 +1,18 @@
+<?php
+
+ include("../include/conexao.php");       
+ 
+ echo "teste";
+       
+//        $sql = ("SELECT * FROM turma WHERE tur_ano='$ano' && tur_semestre='$semestre'");
+//        $rs = mysqli_fetch_assoc ($sql);
+//        // verificar se existem...
+//        $ver = mysqli_num_rows ($sql);
+//        if( $ver <=0 )
+//        {
+//        echo"Inválido";
+//        }else{
+//        echo"Válido";
+
+//        }  
+    
