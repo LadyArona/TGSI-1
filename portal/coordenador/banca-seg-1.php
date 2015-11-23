a4a155f4e9792d949616473a679312514329401d
Juliana da Silva <julianafatsil@mail.ufsm.br>
2015-11-23 04:29:37 -0200

    Atualizações da Banca

--- portal/coordenador/banca-seg-1.php
+++ portal/coordenador/banca-seg-1.php
@@ -3,159 +3,120 @@
     session_start();
     $_SESSION['categoriaPagina'] = 1;
     include("../restrito.php");
+    
     include("cabecalho.php");
     include("../navbar.php");
-     include("../include/funcoes.php");
     include("navbar-coordenador.php");
-   ?>
+    include("../include/funcoes.php");
+    include("../include/conexao.php"); 
 
-    <div class="band">
-            <div class="container">
-                <h2 class="primary stroked-bottom text-shadowed margin-bottom "> Cadastro de Banca</h2>
-            <!--Inicio Caixa de busca Banca-->   
-                    <form id="insereUsuario" action="banca-seg.php" method="post">
-                        <div class="box shadowed bordered rounded">
-                            <div class="row"> 
-                                <div class="span2"> 
-                                    <label class="label" for="ano">Ano<span class="required"></span></label>
-                                        <br>
-                                        <input id="ano" name="ano" class="textfield width-100 integer" type="text" value=""/>  
-                                </div> 
-                                <div class="span3"> 
-                                    <span class="label">Período<span class="required"></span></span>
-                                        <br>
-                                        <select id="periodo" name="periodo" class="textfield width-100"> 
-                                            <option value="101">1. Semestre</option>
-                                            <option value="102">2. Semestre</option>
-                                        </select>
-                                </div>
-                                
-                                 <div class="span2">
-                                     <label class="label" for=""></label><br >
-                                     <button id="buscaBanca" type="submit" class="btn primary small">
-                                        <i class="icon-edit"></i> Cadastrar
-                                     </button> 
-                                 </div>
-                            </div>
-                            
-                            <!--FIM Caixa de busca-->
-                        </div>
-                    </form>
-                        <br> 
- <!-- tabela de alunos -->
-                <div class="bordered rounded diced striped hovered shadowed narrow table">
-                    <table class="bordered rounded diced striped hovered shadowed narrow table">
-                        <caption>Alunos cadastrados</caption>
-                        <thead class="header"> <tr><th WIDTH="80"></th> <th>Participante</th> <th class="hidden-tablet">Categoria</th> </tr> </thead>
-                        <tbody>
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td>
-                                <td>Professor 1</td> 
-                                <td class="hidden-tablet">Coordenador</td>
-                            </tr>
-                            
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td>
-                                <td>Professor 2</td> 
-                                <td class="hidden-tablet">Orientador</td>
-                            </tr>
-                            
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td> 
-                                <td>Professor 3</td> 
-                                <td class="hidden-tablet">Avaliador</td>
-                            </tr>
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td> 
-                                <td>Professor 4</td> 
-                                <td class="hidden-tablet">Avaliador</td>
-                            </tr>
-                            <tr>
-                                <td WIDTH="80">
-                                    <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                        <button type="button" class="btn large"><i class="icon-trash"></i></button>
-                                    </div>
-                                </td> 
-                                <td>Professor 5</td> 
-                                <td class="hidden-tablet">Avaliador</td>
-                            </tr>
-                        </tbody>
-                    </table>
+    if (isset($_POST['ano'])) {
+        $ano      = $mysqli->real_escape_string($_POST['ano']); /*pegando os valores do formulario*/
+        $semestre = $mysqli->real_escape_string($_POST['semestre']);
+    } else {
+        echo "<script>location.href='banca.php';</script>";
+        $mysqli->Close();
+        die();
+    }   
+    
+    /*pega no banco de dados da turma */
+    $query = "SELECT  tur_codigo,tur_ano ,tur_semestre,tur_descricao, tur_data_proposta from turma where tur_ano = '$ano' and tur_semestre = '$semestre'";
+    /*retorna a quantidade registros encontrados na consulta acima */
+    $queryTurma = $mysqli->query($query);
+    
+    /*se quantidade de linhas maior que zero*/
+    if(mysqli_num_rows($queryTurma) > 0){
+        $resultado = $queryTurma->fetch_assoc();
+        $descricao = $resultado['tur_descricao'];
+        $proposta = $resultado['tur_data_proposta'];
+        $codigo = $resultado['tur_codigo'];
+    } else {
+        $descricao = '';
+        $proposta = '';
+        $codigo = '';            
+    }
+     
+    ?>  
+ <div class="band">
+    <div class="container">
+        <h2 class="primary stroked-bottom text-shadowed margin-bottom "> Cadastro de Banca - Busca de alunos</h2>
+        <!--envia dados para turma-aluno-insere.php ao clicar em Inserir-->
+        <form id="BuscaTurma" action="turma-seg.php" method="post">
+            <div class="box shadowed bordered rounded">
+                <div class="row">
+                    <div class="span12">
+                        
+        <?php
+            if (($codigo >= 0) && ($codigo != '')){          
+                /*pega no banco de dados da turma */
+                $sqlTurmaAluno = "SELECT `tud_codigo`, `usu_aluno`, `usu_orientador`, `usu_coorientador`, `tud_titulo`
+                          FROM `turma_detalhe` WHERE `tur_codigo` = $codigo";
+                /*retorna a quantidade registros encontrados na consulta acima */
+                $queryTurmaAluno = $mysqli->query($sqlTurmaAluno);
 
-                <div id="paginationWrapper"> 
-                <table class="bordered rounded diced striped hovered shadowed narrow table"> 
-                    <thead class="header"> 
-                        <tr> 
-                            <th WIDTH="150">Buscar</th> 
-                            <th WIDTH="150">Matrícula</th> 
-                            <th>Nome</th> 
-                            <th>Orientador</th> 
-                            <th>Coorientador</th> 
-                            <th WIDTH="100">Título TGSI</th> 
-                        </tr> 
-                    </thead> 
-                    
-                    <tbody> 
-                        <tr data-role="tableRow" data-id=""> 
-                            <td 
-                                <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                </div></td> 
-                            <td WIDTH="150">201509013</td>
-                            <td>Nome do Aluno1</td> 
-                            <td>Orientador Aluno</td> 
-                            <td>Coorientador Aluno</td> 
-                            <td WIDTH="100">Titulo TGSI</td> 
-                        
-                    </tr>
-                        <tr data-role="tableRow" data-id=""> 
-                            <td 
-                                <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                </div></td> 
-                            <td WIDTH="150">208969013</td>
-                            <td>Nome do Aluno2</td> 
-                            <td>Orientador Aluno</td> 
-                            <td>Coorientador Aluno</td> 
-                            <td WIDTH="100">Titulo TGSI</td> 
-                        </tr>
-                        
-                        <tr data-role="tableRow" data-id=""> 
-                            <td 
-                                <div class="btn-group mini">
-                                        <button type="button" class="btn"><i class="icon-edit"></i></button>
-                                </div></td> 
-                            <td WIDTH="100">201509013</td>
-                            <td>Nome do Aluno3</td> 
-                            <td>Orientador Aluno</td> 
-                            <td>Coorientador Aluno</td> 
-                            <td WIDTH="150">Titulo TGSI</td> 
-                        </tr>
-                    </tbody> 
-                </table> 
-            </div>                    
-                </div>
- 
- 
- 
+                /*se quantidade de linhas maior que zero*/
+                if(mysqli_num_rows($queryTurmaAluno) > 0){
+                    echo '<br>';
+                    echo '<div id="paginationWrapper">'; 
+                    echo '    <table class="bordered rounded diced striped hovered shadowed narrow table">'; 
+                    echo '        <thead class="header">'; 
+                    echo '            <tr>'; 
+                    echo '                <th WIDTH="25%">Aluno</th>'; 
+                    echo '                <th WIDTH="23%">Orientador</th>'; 
+                    echo '                <th WIDTH="25%">Título TGSI</th>';
+                    echo '                <th WIDTH="100"></th>';
+                    echo '            </tr>'; 
+                    echo '        </thead>';                     
+                    echo '        <tbody>';                
+                    while ($Linha = $queryTurmaAluno->fetch_assoc()) { 
+                        echo '        <tr data-role="tableRow" data-id="">'; 
+                        echo '            <td WIDTH="25%">';
+                        echo BuscaDado('usu_nome', 'usuario', 'usu_codigo = '.$Linha['usu_aluno']);
+                        echo              '</td>'; 
+                        echo '            <td WIDTH="23%">';
+                        echo BuscaDado('usu_nome', 'usuario', 'usu_codigo = '.$Linha['usu_orientador']);
+                        echo              '</td>'; 
+                        echo '            <td WIDTH="25%">'.$Linha['tud_titulo'].'</td>';
+                        echo              '</td>';
+                        echo              '<td WIDTH="100"><div class="btn-group small">';
+                        echo              '<button type="button" class="btn primary" onclick="Cadastrar()"><i class="icon-edit"></i> Cadastrar</button></div></td>';
+                        echo '        </tr>';
+                    } 
+   
+                    echo '        </tbody>'; 
+                    echo '    </table>'; 
+                    echo '</div>'; 
+                } else {
+                    echo "<div class='row'><div class='span8'><div class='box warning";
+                    echo "'><button type='button' class='close' data-dismiss='box'>&times;</button>Turma nao possui alunos.";
+                    echo "</div></div></div>";
+                    echo '<button class="btn left" id="voltar" name="voltar" type="button" onclick="parent.location=\'banca.php\'">'; 
+                    echo '    <i class="icon-arrow-left"></i> Voltar</button>';                    
+                    //echo "Turma nao possui alunos.";
+                }
+            } else {
+                //função recebe mensagem
+                echo "<div class='row'><div class='span8'><div class='box error";
+                echo "'><button type='button' class='close' data-dismiss='box'>&times;</button>Turma não existe.";
+                echo "</div></div></div>";
+                echo '<button class="btn left" id="voltar" name="voltar" type="button" onclick="parent.location=\'banca.php\'">'; 
+                echo '    <i class="icon-arrow-left"></i> Voltar</button>';                  
+            }
+        ?>      
+    </div>
+</div>
+
+<script>
+    function Cadastrar()
+    {
+        location.href="banca-seg.php";
+    }
+</script>
+<br>
+
+<?php
+    include("../rodape.php");
+    $mysqli->Close();
+?>
+
+
